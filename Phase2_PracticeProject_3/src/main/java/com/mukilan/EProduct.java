84cb9b6eca7e247fb0709be25ab69e7bcdc78818
mukil <mukil@murugamanisubbaiah>
2022-02-03 08:20:10 +0530

    proj_3

---
+++ Phase2_PracticeProject_3/src/main/java/com/mukilan/EProduct.java
@@ -0,0 +1,49 @@
+package com.mukilan;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="eproduct")
+public class EProduct {
+
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="price")
+	private Double price;
+
+	
+	//ALt + S + R
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
